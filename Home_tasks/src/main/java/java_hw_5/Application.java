45b72162317fd02e81b2b7eb8c57e9193094d489
Yuliia Mostova <mostic2026@gmail.com>
2026-05-25 16:48:30 +0300

    Home work after lesson 6

---
+++ Home_tasks/src/main/java/java_hw_5/Application.java
@@ -0,0 +1,11 @@
+package java_hw_5;
+import java.util.Scanner;
+public class Application {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter your name:");
+        String playerName = scanner.nextLine();
+        NumbersApplication game = new NumbersApplication(playerName);
+        game.startGame(scanner);
+            }
+        }
---
+++ Home_tasks/src/main/java/java_hw_5/NumbersApplication.java
@@ -0,0 +1,27 @@
+package java_hw_5;
+import java.util.Random;
+    import java.util.Scanner;
+    public class NumbersApplication {
+        private String name;
+        private int gameNumber;
+        public NumbersApplication(String name) {
+            this.name = name;
+            Random random = new Random();
+            this.gameNumber = random.nextInt(101);
+        }
+        public void startGame(Scanner scanner) {
+            System.out.println("Let the game begin!");
+            while (true) {
+                System.out.println("Please, enter your number:");
+                int userNumber = scanner.nextInt();
+                if (userNumber < gameNumber) {
+                    System.out.println("Your number is too small. Please, try again.");
+                } else if (userNumber > gameNumber) {
+                    System.out.println("Your number is too big. Please, try again.");
+                } else {
+                    System.out.println("Congratulations, " + name + "!");
+                    break;
+                }
+            }
+        }
+    }
